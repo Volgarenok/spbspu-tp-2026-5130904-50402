a7b4687b7eb0db0342e088a3953304ea367e2f3c
jar <Jarafan3@gmail.com>
2026-03-29 13:52:57 +0300

    Wached video

--- afanasev.yaroslav/T1/main.cpp
+++ afanasev.yaroslav/T1/main.cpp
@@ -1,8 +1,43 @@
 #include <iostream>
 
+#include <map>
+#include <unordered_map>
+
+void hi()
+{
+  std::cout << "aaaaaaaaaAAAAAAAAAAaaaaaaaa" << '\n';
+}
+
 int main()
 {
-	std::s
+  std::string cmd;
+
+  while (std::cin >> cmd)
+  {
+//    std::cout << cmd << '\n';
+
+    if (cmd == "hello")
+    {
+      hi();
+    }
+    else
+    {
+      std::cout << "not have this command" << '\n';
+    }
+    
+  }
+
+  if (!std::cin.eof())
+  {
+    std::cerr << "Error input\n";
+    return 1;
+  }
+  
+  
+
+
+  int a;
+  std::cin >> a;
 
   return 0;
 }
