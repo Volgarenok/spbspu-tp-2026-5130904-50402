967a989bff6b92c53109509515ca0f363bdb0004
lavrentev.makar <makar.lavrentev@yandex.ru>
2026-04-01 18:16:32 +0300

    Initialized functions for notes and map of funcs in main.cpp

--- lavrentev.makar/T1/Commands.hpp
+++ lavrentev.makar/T1/Commands.hpp
@@ -2,7 +2,21 @@
 #define COMMANDS_HPP
 #include <iostream>
 #include <iomanip>
+#include "Note.hpp"
 
-using cmd_t = void(*)(std::istream&, std::ostream&, std::vector< std::string >&);
+using cmd_t = void(*)(std::istream&, std::ostream&, std::vector< lavrentev::Note >&);
+
+namespace lavrentev{
+  void note(std::istream& in, std::ostream&, std::vector< lavrentev::Note >& db);
+  void line(std::istream& in, std::ostream&, std::vector< lavrentev::Note >& db);
+  void show(std::istream& in, std::ostream& out, std::vector< lavrentev::Note >& db);
+  void drop(std::istream& in, std::ostream&, std::vector< lavrentev::Note >& db);
+
+  void link(std::istream& in, std::ostream&, std::vector< lavrentev::Note >& db);
+  void halt(std::istream& in, std::ostream&, std::vector< lavrentev::Note >& db);
+  void mind(std::istream& in, std::ostream& out, std::vector< lavrentev::Note >& db);
+  void expired(std::istream& in, std::ostream& out, std::vector< lavrentev::Note >& db);
+  void refresh(std::istream& in, std::ostream&, std::vector< lavrentev::Note >& db);
+}
 
 #endif
--- lavrentev.makar/T1/main.cpp
+++ lavrentev.makar/T1/main.cpp
@@ -5,18 +5,23 @@
 int main()
 {
   std::vector< lavrentev::Note > db;
-  using cmd_t = void(*)(std::istream&, std::ostream&, std::vector< std::string >&);
+  //using cmd_t = void(*)(std::istream&, std::ostream&, std::vector< std::string >&);
   std::unordered_map< std::string, cmd_t > cmds;
 
-  cmds["add"] = lavrentev::add_string;
-  cmds["show-last"] = lavrentev::show_last;
-  cmds["hello"] = lavrentev::hi;
-  cmds["noop"] = lavrentev::noop;
+  cmds["note"] = lavrentev::note;
+  cmds["line"] = lavrentev::line;
+  cmds["show"] = lavrentev::show;
+  cmds["drop"] = lavrentev::drop;
+  cmds["link"] = lavrentev::link;
+  cmds["halt"] = lavrentev::halt;
+  cmds["mind"] = lavrentev::mind;
+  cmds["expired"] = lavrentev::expired;
+  cmds["refresh"] = lavrentev::refresh;
 
   std::string cmd;
   while (std::cin >> cmd) {
     try {
-      //cmds.at(cmd)(std::cin, std::cout, db);
+      cmds.at(cmd)(std::cin, std::cout, db);
     } catch (const std::out_of_range&) {
       std::cout << "<INVALID COMMAND>\n";
       auto toignore = std::numeric_limits< std::streamsize >::max();
