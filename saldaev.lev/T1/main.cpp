8ceed974862407c596eb78b3e6e96a02a7cffbbb
Carrot_lord <lev.saldaev@gmai.com>
2026-04-01 14:28:30 +0300

    Implement Note class interface

--- saldaev.lev/T1/main.cpp
+++ saldaev.lev/T1/main.cpp
@@ -1,2 +1,29 @@
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace saldaev
+{
+  struct Note
+  {
+    std::string name;
+
+    Note(std::string name);
+    ~Note();
+
+    void addLine(std::string line);
+    void addLink(std::shared_ptr< Note > note);
+    void removeLink(const std::string &noteName);
+
+    std::vector< std::weak_ptr< Note > > &getAllLinks();
+    std::vector< std::shared_ptr< Note > > getActiveLinks();
+    void show();
+
+  private:
+    std::vector< std::string > lines;
+    std::vector< std::weak_ptr< Note > > links;
+  };
+
+}
 int main()
 {}
