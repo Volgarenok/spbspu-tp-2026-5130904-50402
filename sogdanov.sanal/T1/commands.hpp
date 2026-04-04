b0d5a839130509031461ae07259116bd1520c921
kuzRK <sanalsogdanov11188@gmail.com>
2026-04-04 15:10:53 +0300

    commands_hpp implementation

---
+++ sogdanov.sanal/T1/commands.hpp
@@ -0,0 +1,20 @@
+#ifndef COMMANDS_HPP
+#define COMMANDS_HPP
+#include <iostream>
+#include <unordered_map>
+#include "note.hpp"
+namespace sogdanov
+{
+  using NotePtr = std::shared_ptr<Note>;
+  using NoteMap = std::unordered_map<std::string, NotePtr>;
+  void cmd_note(std::istream &in, std::ostream &out, NoteMap &notes);
+  void cmd_line(std::istream &in, std::ostream &out, NoteMap &notes);
+  void cmd_show(std::istream &in, std::ostream &out, NoteMap &notes);
+  void cmd_drop(std::istream &in, std::ostream &out, NoteMap &notes);
+  void cmd_link(std::istream &in, std::ostream &out, NoteMap &notes);
+  void cmd_halt(std::istream &in, std::ostream &out, NoteMap &notes);
+  void cmd_mind(std::istream &in, std::ostream &out, NoteMap &notes);
+  void cmd_expired(std::istream &in, std::ostream &out, NoteMap &notes);
+  void cmd_refresh(std::istream &in, std::ostream &out, NoteMap &notes);
+}
+#endif
