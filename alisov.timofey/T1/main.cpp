40fa4152eba2723cdcf1dc05fb51762f99b5bf06
AlisovTimofey <timofeyalisov@gmail.com>
2026-04-04 23:12:45 +0300

    finish main ig

--- alisov.timofey/T1/main.cpp
+++ alisov.timofey/T1/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <string>
+#include <ostream>
+#include <string>
 #include <unordered_map>
 
 namespace alisov
@@ -29,4 +32,35 @@ namespace alisov
 
 int main()
 {
+  std::unordered_map< std::string, alisov::Note > notes;
+  using cmd1 =
+      void (*)(std::istream &, std::ostream &, std::unordered_map< std::string, alisov::Note >);
+  std::unordered_map< std::string, cmd1 > cmds;
+  cmds["note"] = alisov::note;
+  cmds["line"] = alisov::line;
+  cmds["show"] = alisov::show;
+  cmds["drop"] = alisov::drop;
+  cmds["link"] = alisov::link;
+  cmds["halt"] = alisov::halt;
+  cmds["mind"] = alisov::mind;
+  cmds["expired"] = alisov::expired;
+  cmds["refresh"] = alisov::refresh;
+
+  std::string cmd;
+  while (std::cin >> cmd)
+  {
+    try
+    {
+      cmds.at(cmd)(std::cin, std::cout, notes);
+    }
+    catch (std::out_of_range &e)
+    {
+      std::cout << "Invalid command\n";
+    }
+    if (!std::cin.eof())
+    {
+      std::cerr << "Bad input\n";
+      return 1;
+    }
+  }
 }
