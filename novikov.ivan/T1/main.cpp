1f4ae225dbd56ec9aaa407f280b128cabc6112e2
Kayv1e <krutojpacanivan@gmail.com>
2026-04-04 17:42:00 +0300

    Unordered map alias and note structure definement

--- novikov.ivan/T1/main.cpp
+++ novikov.ivan/T1/main.cpp
@@ -2,29 +2,40 @@
 #include <string>
 #include <unordered_map>
 #include <limits>
+#include <memory>
+#include <vector>
+
+namespace novikov
+{
+  struct Note;
+}
+
+using map_t = std::unordered_map< std::string, std::shared_ptr< novikov::Note > >;
 
 namespace novikov
 {
   struct Note
   {
+    std::vector< std::string > entries;
+    std::vector< std::weak_ptr< Note > > links;
   };
 
-  void note(std::istream& in, std::ostream&, std::unordered_map< std::string, novikov::Note > notes);
-  void line(std::istream& in, std::ostream&, std::unordered_map< std::string, novikov::Note > notes);
-  void show(std::istream& in, std::ostream& out, std::unordered_map< std::string, novikov::Note > notes);
-  void drop(std::istream& in, std::ostream&, std::unordered_map< std::string, novikov::Note > notes);
-  void link(std::istream& in, std::ostream&, std::unordered_map< std::string, novikov::Note > notes);
-  void halt(std::istream& in, std::ostream&, std::unordered_map< std::string, novikov::Note > notes);
-  void mind(std::istream& in, std::ostream& out, std::unordered_map< std::string, novikov::Note > notes);
-  void expired(std::istream& in, std::ostream& out, std::unordered_map< std::string, novikov::Note > notes);
-  void refresh(std::istream& in, std::ostream&, std::unordered_map< std::string, novikov::Note > notes);
+  void note(std::istream& in, std::ostream&, map_t notes);
+  void line(std::istream& in, std::ostream&, map_t notes);
+  void show(std::istream& in, std::ostream& out, map_t notes);
+  void drop(std::istream& in, std::ostream&, map_t notes);
+  void link(std::istream& in, std::ostream&, map_t notes);
+  void halt(std::istream& in, std::ostream&, map_t notes);
+  void mind(std::istream& in, std::ostream& out, map_t notes);
+  void expired(std::istream& in, std::ostream& out, map_t notes);
+  void refresh(std::istream& in, std::ostream&, map_t notes);
 }
 
 int main()
 {
-  std::unordered_map< std::string, novikov::Note > notes;
+  map_t notes;
 
-  using cmd_t = void (*)(std::istream&, std::ostream&, std::unordered_map< std::string, novikov::Note >);
+  using cmd_t = void (*)(std::istream&, std::ostream&, map_t);
   std::unordered_map< std::string, cmd_t > cmds;
   cmds["note"] = novikov::note;
   cmds["line"] = novikov::line;
