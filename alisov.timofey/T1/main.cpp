036250427da6dfb849af73019653ab4c6a283659
AlisovTimofey <timofeyalisov@gmail.com>
2026-04-04 22:55:35 +0300

    init command list

--- alisov.timofey/T1/main.cpp
+++ alisov.timofey/T1/main.cpp
@@ -1,3 +1,32 @@
+#include <iostream>
+#include <unordered_map>
+
+namespace alisov
+{
+  struct Note
+  {
+  };
+
+  void note(std::istream &in, std::ostream &out,
+            std::unordered_map< std::string, alisov::Note > notes);
+  void line(std::istream &in, std::ostream &out,
+            std::unordered_map< std::string, alisov::Note > notes);
+  void show(std::istream &in, std::ostream &out,
+            std::unordered_map< std::string, alisov::Note > notes);
+  void drop(std::istream &in, std::ostream &out,
+            std::unordered_map< std::string, alisov::Note > notes);
+  void link(std::istream &in, std::ostream &out,
+            std::unordered_map< std::string, alisov::Note > notes);
+  void halt(std::istream &in, std::ostream &out,
+            std::unordered_map< std::string, alisov::Note > notes);
+  void mind(std::istream &in, std::ostream &out,
+            std::unordered_map< std::string, alisov::Note > notes);
+  void expired(std::istream &in, std::ostream &out,
+               std::unordered_map< std::string, alisov::Note > notes);
+  void refresh(std::istream &in, std::ostream &out,
+               std::unordered_map< std::string, alisov::Note > notes);
+}
+
 int main()
 {
 }
