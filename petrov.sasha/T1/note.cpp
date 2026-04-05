042ea25fe1aab95462a6135692e4ff34e2fa5952
sp0146001 <sp0146001@gmail.com>
2026-04-05 19:38:50 +0300

    link find iter + add one note function

--- petrov.sasha/T1/note.cpp
+++ petrov.sasha/T1/note.cpp
@@ -1,1 +1,37 @@
 #include "note.hpp"
+#include <iomanip>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+petrov::Note::Note(std::string name):
+  name(name),
+  desc(),
+  links()
+{}
+
+petrov::linkIt_t petrov::find(linkIt_t start, linkIt_t end, std::string name)
+{
+  for (; start != end; ++start) {
+    if (!start->expired() && start->lock()->name == name) {
+      return start;
+    }
+  }
+  return end;
+}
+
+void petrov::addNote(std::istream &is, std::ostream &, notes_t &db)
+{
+  std::string name;
+  is >> name;
+  if (db.find(name) == db.end()) {
+    db[name] = std::make_shared< Note >(name);
+  } else {
+    throw std::logic_error("This note already exists");
+  }
+}
+
--- petrov.sasha/T1/note.hpp
+++ petrov.sasha/T1/note.hpp
@@ -1,6 +1,5 @@
 #ifndef NOTE_HPP
 #define NOTE_HPP
-
 #include <iosfwd>
 #include <limits>
 #include <memory>
@@ -8,19 +7,23 @@
 #include <unordered_map>
 #include <vector>
 
-namespace petrov {
-  class Note {
+namespace petrov
+{
+  class Note
+  {
   public:
     Note(std::string name);
     std::string name;
     std::vector< std::string > desc;
     std::vector< std::weak_ptr< Note > > links;
   };
-  
   constexpr std::streamsize streamMax = std::numeric_limits< std::streamsize >::max();
   using notes_t = std::unordered_map< std::string, std::shared_ptr< Note > >;
-  using func_t = void (*)(std::istream& , std::ostream& , notes_t&);
+  using func_t = void (*)(std::istream &, std::ostream &, notes_t &);
+  
+  void addNote(std::istream &is, std::ostream &, notes_t &db);
+  using linkIt_t = std::vector< std::weak_ptr< Note > >::iterator;
+  linkIt_t find(linkIt_t, linkIt_t, std::string name);
 }
-
 #endif
 
