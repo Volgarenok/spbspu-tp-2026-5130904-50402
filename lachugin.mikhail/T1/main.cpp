457c48f32fbbdee0c4de100a5e94e3c253fc5d76
Михаил Лачугин <mikee@MacBook-Air-Mihail.local>
2026-04-14 01:10:05 +0300

    fix catch again

--- lachugin.mikhail/T1/main.cpp
+++ lachugin.mikhail/T1/main.cpp
@@ -19,23 +19,14 @@ int main() {
 
   std::string cmd;
 
-  while (std::cin >> cmd)
-  {
-    try
-    {
+  while (std::cin >> cmd) {
+    try {
       cmds.at(cmd)(std::cin, std::cout, db);
-    }
-    catch (const std::out_of_range&)
-    {
+    } catch (const std::out_of_range& ) {
       std::cout << "<INVALID COMMAND>\n";
-      auto toignore = std::numeric_limits<std::streamsize>::max();
-      std::cin.ignore(toignore, '\n');
-    }
-    catch (const std::logic_error&)
-    {
+      std::cin.ignore(std::numeric_limits< std::streamsize>::max(), '\n');
+    } catch (const std::logic_error& e) {
       std::cout << "<INVALID COMMAND>\n";
-      auto toignore = std::numeric_limits<std::streamsize>::max();
-      std::cin.ignore(toignore, '\n');
     }
   }
 
