5daa0d378c707f71e1786191b429cfed92e3d8b3
Михаил Лачугин <mikee@MacBook-Air-Mihail.local>
2026-04-14 00:03:19 +0300

    add all operations

--- lachugin.mikhail/T1/operations.cpp
+++ lachugin.mikhail/T1/operations.cpp
@@ -52,3 +52,140 @@ void lachugin::showNote(std::istream &in, std::ostream &out, notesMap &db)
 }
 
 
+void lachugin::addLink(std::istream &in, std::ostream &, notesMap &db)
+{
+  std::string note1, note2;
+  in >> note1 >> note2;
+
+  auto it1 = db.find(note1);
+  auto it2 = db.find(note2);
+  if (it1 == db.end() || it2 == db.end())
+  {
+    throw std::logic_error ("Err: note not exist");
+  }
+
+  for (size_t i = 0; i < it1->second->links.size(); ++i)
+  {
+    auto sp = it1->second->links[i].lock();
+
+    if (sp && sp == it2->second)
+    {
+      throw std::logic_error("duplicate");
+    }
+  }
+
+  it1->second->links.push_back(it2->second);
+}
+
+void lachugin::removeNote(std::istream &in, std::ostream &, notesMap &db)
+{
+  std::string note;
+  in >> note;
+
+  auto it = db.find(note);
+  if (it == db.end())
+  {
+    throw std::logic_error ("Err: note not exist");
+  }
+
+  db.erase(note);
+}
+
+void lachugin::removeLink(std::istream &in, std::ostream &, notesMap &db)
+{
+  std::string note1, note2;
+  in >> note1 >> note2;
+
+  auto it1 = db.find(note1);
+  auto it2 = db.find(note2);
+  if (it1 == db.end() || it2 == db.end())
+  {
+    throw std::logic_error ("Err: note not exist");
+  }
+
+  for (size_t i = 0; i < it1->second->links.size(); ++i)
+  {
+    auto sp = it1->second->links[i].lock();
+
+    if (sp && sp == it2->second)
+    {
+      it1->second->links.erase(it1->second->links.begin() + i);
+      return;
+    }
+  }
+}
+
+void lachugin::printLinks(std::istream &in, std::ostream &out, notesMap &db)
+{
+  std::string note;
+  in >> note;
+
+  auto it = db.find(note);
+  if (it == db.end())
+  {
+    throw std::logic_error ("Err: note not exist");
+  }
+
+  for (size_t i = 0; i < it->second->links.size(); ++i)
+  {
+    auto sp = it->second->links[i].lock();
+    if (sp)
+    {
+      out << sp->name << "\n";
+    }
+  }
+}
+
+void lachugin::allRemovedNotes(std::istream &in, std::ostream &out, notesMap &db)
+{
+  std::string note;
+  in >> note;
+
+  auto it = db.find(note);
+  if (it == db.end())
+  {
+    throw std::logic_error ("Err: note not exist");
+  }
+
+  size_t count = 0;
+
+  for (size_t i = 0; i < it->second->links.size(); ++i)
+  {
+    auto sp = it->second->links[i].lock();
+    if (!sp)
+    {
+      ++count;
+    }
+  }
+
+  out << count << "\n";
+}
+
+void lachugin::linksRemover(std::istream &in, std::ostream &, notesMap &db)
+{
+  std::string note;
+  in >> note;
+
+  auto it = db.find(note);
+  if (it == db.end())
+  {
+    throw std::logic_error ("Err: note not exist");
+  }
+
+  size_t i = 0;
+
+  while (i < it->second->links.size())
+  {
+    auto sp = it->second->links[i].lock();
+
+    if (!sp)
+    {
+      it->second->links.erase(it->second->links.begin() + i);
+    }
+    else
+    {
+      ++i;
+    }
+  }
+}
+
