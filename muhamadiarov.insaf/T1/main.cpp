b5e300d487d6c3abc2bf2dde3a9d6b0dc8d03db8
InsafM1 <muhamadiarovinsaf0@gmail.com>
2026-04-28 16:18:44 +0300

    Correct output message about errors

--- muhamadiarov.insaf/T1/main.cpp
+++ muhamadiarov.insaf/T1/main.cpp
@@ -28,9 +28,9 @@ int main()
     {
       cmds.at(str)(std::cin, std::cout, result);
     }
-    catch (const std::exception &e)
+    catch (const std::exception&)
     {
-      std::cerr << "<INVALID COMMAND: " << e.what() << ">\n";
+      std::cerr << "<INVALID COMMAND>\n";
       std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
     }
   }
