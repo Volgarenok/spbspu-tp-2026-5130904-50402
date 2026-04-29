14b083edf48ecb2712d1f6f06f9b072f09ac0a39
EGGorkaa <egor_novikov20004@mail.ru>
2026-04-29 21:36:27 +0300

    fix error output

--- novikov.egor/T1/main.cpp
+++ novikov.egor/T1/main.cpp
@@ -25,8 +25,8 @@ int main()
       std::cout << "<INVALID COMMAND>\n";
       auto toignore = std::numeric_limits< std::streamsize >::max();
       std::cin.ignore(toignore, '\n');
-    } catch (const std::logic_error &e) {
-      std::cout << "<INVALID COMMAND " << e.what() << ">\n";
+    } catch (const std::logic_error&) {
+      std::cout << "<INVALID COMMAND>\n";
       auto toignore = std::numeric_limits< std::streamsize >::max();
       std::cin.ignore(toignore, '\n');
     }
