c3aeff6551518d8a4e0afe97aaf850f54d1bc409
lavrentev.makar <makar.lavrentev@yandex.ru>
2026-05-22 23:57:15 +0300

    Realized 'main'

--- lavrentev.makar/T2/main.cpp
+++ lavrentev.makar/T2/main.cpp
@@ -1,4 +1,5 @@
-#include <cstddef>
+#include <iostream>
+#include <iterator>
 #include <vector>
 #include "DataStruct.hpp"
 
@@ -6,4 +7,11 @@ int main()
 {
   using T = lavrentev::DataStruct;
   std::vector< T > data;
+  using in_t = std::istream_iterator< T >;
+
+  std::copy(in_t{std::cin}, in_t{}, std::back_inserter(data));
+  std::sort(data.begin(), data.end());
+
+  using out_t = std::ostream_iterator< T >;
+  std::copy(data.begin(), data.end(), out_t{std::cout, " "});
 }
