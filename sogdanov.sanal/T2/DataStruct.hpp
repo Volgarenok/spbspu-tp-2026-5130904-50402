a30da6349076932a76f0c873ff449cb1d42c3e2d
kuzRK <sanalsogdanov11188@gmail.com>
2026-05-23 18:25:33 +0300

    add DataStruct decl and operators

---
+++ sogdanov.sanal/T2/DataStruct.hpp
@@ -0,0 +1,21 @@
+#ifndef DATASTRUCT_HPP
+#define DATASTRUCT_HPP
+
+#include <string>
+#include <iostream>
+
+namespace sogdanov
+{
+  struct DataStruct
+  {
+    double key1;
+    long long key2;
+    std::string key3;
+  };
+
+  std::istream& operator>>(std::istream& in, DataStruct& dest);
+  std::ostream& operator<<(std::ostream& out, const DataStruct& src);
+  bool operator<(const DataStruct& lhs, const DataStruct& rhs);
+}
+
+#endif
