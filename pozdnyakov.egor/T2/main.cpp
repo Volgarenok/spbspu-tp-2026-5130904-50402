d6e3c35e60140e531eda352a201f1b583bf3c289
R0me0Star <eip_spb@mail.ru>
2026-05-23 20:17:11 +0300

    main

--- pozdnyakov.egor/T2/main.cpp
+++ pozdnyakov.egor/T2/main.cpp
@@ -1,2 +1,26 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <vector>
+#include "data.hpp"
+
 int main()
-{}
+{
+  std::vector< pozdnyakov::DataStruct > data;
+
+  while (!std::cin.eof()) {
+    std::copy(std::istream_iterator< pozdnyakov::DataStruct >(std::cin),
+              std::istream_iterator< pozdnyakov::DataStruct >(), std::back_inserter(data));
+
+    if (std::cin.fail() && !std::cin.eof()) {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+  }
+
+  std::sort(data.begin(), data.end());
+  std::copy(data.begin(), data.end(), std::ostream_iterator< pozdnyakov::DataStruct >(std::cout, "\n"));
+
+  return 0;
+}
