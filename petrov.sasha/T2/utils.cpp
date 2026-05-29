b742b5e266a4b5c240eedc9617f2ad562d49fe34
sp0146001 <sp0146001@gmail.com>
2026-05-30 01:26:11 +0300

    финальный оператор сборки всех данных-ключей, обрабатывает ( ) и присваивает, если все в порядке

--- petrov.sasha/T2/utils.cpp
+++ petrov.sasha/T2/utils.cpp
@@ -25,11 +25,8 @@ namespace petrov
     if (!sentry) {
       return in;
     }
-
     IOGuard guard(in);
-
     in >> DelimIO{'0'} >> std::oct >> dest.ref;
-
     return in;
   }
 
@@ -39,21 +36,15 @@ namespace petrov
     if (!sentry) {
       return in;
     }
-
     IOGuard guard(in);
-
     char zero = '\0';
     char x = '\0';
-
     in >> zero >> x;
-
     if (!in || zero != '0' || (x != 'x' && x != 'X')) {
       in.setstate(std::ios::failbit);
       return in;
     }
-
     in >> std::hex >> dest.ref;
-
     return in;
   }
 
@@ -63,8 +54,49 @@ namespace petrov
     if (!sentry) {
       return in;
     }
-
     return in >> std::quoted(dest.ref);
+  }
+
+  std::istream &operator>>(std::istream &in, DataStruct &dest)
+  {
+    std::istream::sentry sentry(in);
+    if (!sentry) {
+      return in;
+    }
+    DataStruct temp{};
+    bool got1 = false;
+    bool got2 = false;
+    bool got3 = false;
+    in >> DelimIO{'('};
+    while ((!got1 || !got2 || !got3) && in) {
+      std::string label;
+      in >> label;
+      if (label == ":key1" && !got1) {
+        in >> OctIO{temp.key1};
+        if (in) {
+          got1 = true;
+        }
+      } else if (label == ":key2" && !got2) {
+        in >> HexIO{temp.key2};
+        if (in) {
+          got2 = true;
+        }
+      } else if (label == ":key3" && !got3) {
+        in >> StringIO{temp.key3};
+        if (in) {
+          got3 = true;
+        }
+      } else {
+        in.setstate(std::ios::failbit);
+      }
+    }
+    in >> DelimIO{':'} >> DelimIO{')'};
+    if (in && got1 && got2 && got3) {
+      dest = temp;
+    } else {
+      in.setstate(std::ios::failbit);
+    }
+    return in;
   }
 }
 
