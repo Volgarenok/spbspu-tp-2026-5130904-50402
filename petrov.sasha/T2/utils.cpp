d70d9a9f47301b85134c8e7ca01d20f9cf96c803
sp0146001 <sp0146001@gmail.com>
2026-05-30 01:14:09 +0300

    добавил два оператора ввода для двоих вариантов. в 16ричн. доп проверка

--- petrov.sasha/T2/utils.cpp
+++ petrov.sasha/T2/utils.cpp
@@ -18,5 +18,43 @@ namespace petrov
     }
     return in;
   }
+
+  std::istream &operator>>(std::istream &in, OctIO &&dest)
+  {
+    std::istream::sentry sentry(in);
+    if (!sentry) {
+      return in;
+    }
+
+    IOGuard guard(in);
+
+    in >> DelimIO{'0'} >> std::oct >> dest.ref;
+
+    return in;
+  }
+
+  std::istream &operator>>(std::istream &in, HexIO &&dest)
+  {
+    std::istream::sentry sentry(in);
+    if (!sentry) {
+      return in;
+    }
+
+    IOGuard guard(in);
+
+    char zero = '\0';
+    char x = '\0';
+
+    in >> zero >> x;
+
+    if (!in || zero != '0' || (x != 'x' && x != 'X')) {
+      in.setstate(std::ios::failbit);
+      return in;
+    }
+
+    in >> std::hex >> dest.ref;
+
+    return in;
+  }
 }
 
