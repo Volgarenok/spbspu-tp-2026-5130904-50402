f228d65781ae0858b7b723597b2c513507f5ec70
sp0146001 <sp0146001@gmail.com>
2026-05-30 01:35:01 +0300

    чет забыл его добавить, но это настроойка флагов потока

---
+++ petrov.sasha/T2/ioguard.hpp
@@ -0,0 +1,23 @@
+#ifndef IOGUARD_HPP
+#define IOGUARD_HPP
+#include <ios>
+namespace petrov
+{
+  class IOguard
+  {
+  public:
+    explicit IOguard(std::basic_ios< char > &s);
+    ~IOguard();
+
+  private:
+    std::basic_ios< char > &s_;
+    std::streamsize width_;
+    std::streamsize precision_;
+    std::basic_ios< char >::fmtflags fmt_;
+    char fill_;
+  };
+
+}
+
+#endif
+
