59523ec6e8b4d5e805fdcd692ffc9c9c964c9383
sp0146001 <sp0146001@gmail.com>
2026-05-30 13:50:43 +0300

    похоже вьехал, не надо было тонны текста с проверками выводить

--- petrov.sasha/T2/main.cpp
+++ petrov.sasha/T2/main.cpp
@@ -9,26 +9,20 @@
 int main()
 {
   using T = petrov::DataStruct;
-  std::cin >> std::ws;
-  if (std::cin.peek() == std::char_traits< char >::eof()) {
-    std::cout << "Looks like there is no supported record. Cannot determine input. Test skipped\n";
-    return 0;
-  }
+
   std::vector< T > data;
   using iit_t = std::istream_iterator< T >;
+
   while (!std::cin.eof()) {
     std::copy(iit_t{std::cin}, iit_t{}, std::back_inserter(data));
     if (std::cin.fail()) {
       std::cin.clear(std::cin.rdstate() & ~std::ios::failbit);
-      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
   }
-  if (data.empty()) {
-    std::cout << "Atleast one supported record type\n";
-    return 0;
-  }
+
   std::sort(data.begin(), data.end());
+
   using oit_t = std::ostream_iterator< T >;
   std::copy(data.begin(), data.end(), oit_t{std::cout, "\n"});
 }
-
