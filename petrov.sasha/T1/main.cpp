ce561bb26e02a7efff0452b61c3cfeea26898f0d
sp0146001 <sp0146001@gmail.com>
2026-05-30 15:56:32 +0300

    столбик и добавка cin.ignore чтобы чистить поток при неверном вводе

--- petrov.sasha/T1/main.cpp
+++ petrov.sasha/T1/main.cpp
@@ -8,19 +8,26 @@ int main()
 {
   petrov::notes_t db;
   const std::unordered_map< std::string, petrov::func_t > cmds{
-      {"note", petrov::addNote},    {"line", petrov::addDesc},         {"show", petrov::printNote},
-      {"drop", petrov::dropNote},   {"link", petrov::linkNote},        {"halt", petrov::removeLink},
-      {"mind", petrov::printLinks}, {"expired", petrov::countExpired}, {"refresh", petrov::refreshLinks},
+      {"note", petrov::addNote},
+      {"line", petrov::addDesc},
+      {"show", petrov::printNote},
+      {"drop", petrov::dropNote},
+      {"link", petrov::linkNote},
+      {"halt", petrov::removeLink},
+      {"mind", petrov::printLinks},
+      {"expired", petrov::countExpired},
+      {"refresh", petrov::refreshLinks},
   };
   std::string cmd;
   while (std::cin >> cmd) {
     try {
       cmds.at(cmd)(std::cin, std::cout, db);
-    } catch (const std::out_of_range& ) {
+    } catch (const std::out_of_range &) {
       std::cout << "<INVALID COMMAND>\n";
       std::cin.ignore(petrov::streamMax, '\n');
-    } catch (const std::logic_error& e) {
+    } catch (const std::logic_error &) {
       std::cout << "<INVALID COMMAND>\n";
+      std::cin.ignore(petrov::streamMax, '\n');
     }
   }
 }
