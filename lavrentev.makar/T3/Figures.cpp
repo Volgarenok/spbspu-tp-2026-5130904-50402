88527f4f4e719bdb816e360fdbd506d0c0e4f768
lavrentev.makar <makar.lavrentev@yandex.ru>
2026-06-01 20:31:26 +0300

    Realized funcs for area calculating

--- lavrentev.makar/T3/Figures.cpp
+++ lavrentev.makar/T3/Figures.cpp
@@ -1,8 +1,10 @@
 #include <cstddef>
 #include <map>
+#include <numeric>
+#include <cmath>
 #include "Figures.hpp"
 
-void area(std::istream& is, std::vector< Polygon > plgs)
+void lavrentev::area(std::istream& is, std::vector< Polygon > plgs)
 {
   std::map< std::string, void (*)(std::vector< Polygon >) > cmds;
   cmds["EVEN"] = areaEven;
@@ -34,6 +36,53 @@ void area(std::istream& is, std::vector< Polygon > plgs)
   {
     throw std::invalid_argument("Invalid number");
   }
+}
+
+void lavrentev::areaEven(std::vector< Polygon > plgs)
+{
+
+}
+
+const float lavrentev::Polygon::getArea() const
+{
+  using namespace std::placeholders;
+
+  std::vector< Triangle > triangles(points.size() - 2);
+  size_t index = 1;
+  std::generate(
+    triangles.begin(), 
+    triangles.end(), 
+    std::bind(&lavrentev::Polygon::helpArea, std::ref(points), std::ref(index))
+  );
+
+  std::vector< float > areas(triangles.size());
+  std::transform(
+    triangles.begin(),
+    triangles.end(),
+    areas.begin(),
+    std::bind(&lavrentev::Triangle::getArea, _1));
+  float total = std::accumulate(areas.begin(), areas.end(), 0.0f);
+
+  return total;
+}
+
+lavrentev::Triangle lavrentev::Polygon::helpArea(const std::vector< Point >& points, size_t& index)
+{
+  Triangle ans = Triangle(points[0], points[index], points[index + 1]);
+  ++index;
+  return ans;
+}
+
+const float lavrentev::Triangle::getArea() const
+{
+  int x1 = points[0].x;
+  int x2 = points[1].x;
+  int x3 = points[2].x;
+  int y1 = points[0].y;
+  int y2 = points[1].y;
+  int y3 = points[2].y;
+
+  return 0.5 * std::abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2));
 }
 
 
--- lavrentev.makar/T3/Figures.hpp
+++ lavrentev.makar/T3/Figures.hpp
@@ -1,23 +1,42 @@
 #include <cstddef>
 #include <vector>
 
-struct Point
+namespace lavrentev
 {
-  int x, y;
-};
+  struct Point
+  {
+    int x, y;
+  };
 
-struct Polygon
-{
-  std::vector< Point > points;
-};
+  struct Triangle
+  {
+    static const size_t size = 3;
+    std::vector< Point > points = std::vector< Point >(size);
 
-void area(std::istream& is, std::vector< Polygon > plgs);
-void areaEven(std::vector< Polygon > plgs);
-void areaOdd(std::vector< Polygon > plgs);
-void areaMean(std::vector< Polygon > plgs);
-void areaVrtxs(std::vector< Polygon > plgs, size_t n);
-void max(std::istream& is, std::vector< Polygon > plgs);
-void min(std::istream& is, std::vector< Polygon > plgs);
-void count(std::istream& is, std::vector< Polygon > plgs);
-void maxseq(std::istream& is, std::vector< Polygon > plgs);
-void intersections(std::istream& is, std::vector< Polygon > plgs);
+    Triangle(Point a, Point b, Point c):
+      points{a, b, c}
+    {}
+
+    const float getArea() const;
+  };
+
+  struct Polygon
+  {
+    std::vector< Point > points;
+    const float getArea() const;
+
+    private:
+      static Triangle helpArea(const std::vector< Point >& points, size_t& index);
+  };
+
+  void area(std::istream& is, std::vector< Polygon > plgs);
+  void areaEven(std::vector< Polygon > plgs);
+  void areaOdd(std::vector< Polygon > plgs);
+  void areaMean(std::vector< Polygon > plgs);
+  void areaVrtxs(std::vector< Polygon > plgs, size_t n);
+  void max(std::istream& is, std::vector< Polygon > plgs);
+  void min(std::istream& is, std::vector< Polygon > plgs);
+  void count(std::istream& is, std::vector< Polygon > plgs);
+  void maxseq(std::istream& is, std::vector< Polygon > plgs);
+  void intersections(std::istream& is, std::vector< Polygon > plgs);
+}
