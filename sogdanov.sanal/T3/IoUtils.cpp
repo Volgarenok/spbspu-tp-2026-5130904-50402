86018b16e624214e79c22085c6177b9faa0ab45d
kuzRK <sanalsogdanov11188@gmail.com>
2026-06-04 19:19:31 +0300

    Add IOguard and delimiter parser

---
+++ sogdanov.sanal/T3/IoUtils.cpp
@@ -0,0 +1,33 @@
+#include "IoUtils.hpp"
+
+namespace sogdanov {
+
+  std::istream& operator>>(std::istream& in, DelimiterIO&& dest)
+  {
+    std::istream::sentry sentry(in);
+    if (!sentry) {
+      return in;
+    }
+    char c = '0';
+    in >> c;
+    if (in && (c != dest.exp)) {
+      in.setstate(std::ios::failbit);
+    }
+    return in;
+  }
+
+  IoGuard::IoGuard(std::basic_ios<char>& s):
+    s_(s),
+    fill_(s.fill()),
+    precision_(s.precision()),
+    fmt_(s.flags())
+  {}
+
+  IoGuard::~IoGuard()
+  {
+    s_.fill(fill_);
+    s_.precision(precision_);
+    s_.flags(fmt_);
+  }
+
+}
---
+++ sogdanov.sanal/T3/IoUtils.hpp
@@ -0,0 +1,29 @@
+#ifndef IO_UTILS_HPP
+#define IO_UTILS_HPP
+
+#include <istream>
+#include <ios>
+
+namespace sogdanov {
+
+  struct DelimiterIO {
+    char exp;
+  };
+
+  std::istream& operator>>(std::istream& in, DelimiterIO&& dest);
+
+  class IoGuard {
+  public:
+    explicit IoGuard(std::basic_ios<char>& s);
+    ~IoGuard();
+
+  private:
+    std::basic_ios<char>& s_;
+    char fill_;
+    std::streamsize precision_;
+    std::basic_ios<char>::fmtflags fmt_;
+  };
+
+}
+
+#endif
