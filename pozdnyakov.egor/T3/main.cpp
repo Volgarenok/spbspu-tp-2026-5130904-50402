d1d8470e93a27c229ef9d93c10e9a43a4b9f3744
R0me0Star <eip_spb@mail.ru>
2026-06-05 14:29:23 +0300

    base struct & operators

--- pozdnyakov.egor/T3/main.cpp
+++ pozdnyakov.egor/T3/main.cpp
@@ -1,2 +1,31 @@
-int main()
-{}
+#include <vector>
+
+namespace pozdnyakov
+{
+
+  struct Point
+  {
+    int x, y;
+  };
+
+  bool operator==(const Point &lhs, const Point &rhs)
+  {
+    return lhs.x == rhs.x && lhs.y == rhs.y;
+  }
+
+  bool operator!=(const Point &lhs, const Point &rhs)
+  {
+    return !(lhs == rhs);
+  }
+
+  struct Polygon
+  {
+    std::vector< Point > points;
+  };
+
+  bool operator==(const Polygon &lhs, const Polygon &rhs)
+  {
+    return lhs.points == rhs.points;
+  }
+
+}
