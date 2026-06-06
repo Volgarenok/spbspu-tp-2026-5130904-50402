291c841f0e19fb0040c7212a99d7a60f1b38227e
sp0146001 <sp0146001@gmail.com>
2026-06-06 16:28:42 +0300

    Polygon and initialize operator ==

--- petrov.sasha/T3/shapes.hpp
+++ petrov.sasha/T3/shapes.hpp
@@ -8,5 +8,11 @@ namespace petrov
   struct Point
   {
     int x, y;
-  }
+  };
+  bool operator==(const Point& lhs, const Point& rhs);
+
+  struct Polygon
+  {
+    std::vector< Point > points;
+  };
 }
