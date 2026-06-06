0c6f3085e7b0494a4b27c60405cbd07220aeb82d
sp0146001 <sp0146001@gmail.com>
2026-06-06 20:08:18 +0300

    preparing for user commands

--- petrov.sasha/T3/main.cpp
+++ petrov.sasha/T3/main.cpp
@@ -17,11 +17,24 @@ int main(int argc, char** argv)
     std::cerr << "Incorrect parameters\n";
     return 1;
   }
-
+  std::vector< petrov::Polygon > polygons;
   std::ifstream file(argv[1]);
   if (!file)
   {
     std::cerr << "Error: cannot open file\n";
     return 1;
+  }
+
+  constexpr std::streamsize streamMax = std::numeric_limits< std::streamsize >::max();
+  using iit_t = std::istream_iterator< petrov::Polygon >;
+
+  while (!file.eof())
+  {
+    std::copy(iit_t(file), iit_t(), std::back_inserter(polygons));
+    if (!file)
+    {
+      file.clear();
+      file.ignore(streamMax, '\n');
+    }
   }
 }
