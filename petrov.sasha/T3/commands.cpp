30392abb81df48ec4aeaf6fa3db693854a94a42b
sp0146001 <sp0146001@gmail.com>
2026-06-06 19:29:10 +0300

    cmds cpp

---
+++ petrov.sasha/T3/commands.cpp
@@ -0,0 +1,15 @@
+#include "commands.hpp"
+#include <algorithm>
+#include <cctype>
+#include <functional>
+#include <iomanip>
+#include <iterator>
+#include <limits>
+#include <numeric>
+#include <string>
+#include "shapes.hpp"
+
+namespace petrov
+{
+  
+}
