efcaaa3734071482286008d8c059d6215d3f5cb9
sp0146001 <sp0146001@gmail.com>
2026-06-06 20:10:53 +0300

    replace some of { and add reading commands

--- petrov.sasha/T3/main.cpp
+++ petrov.sasha/T3/main.cpp
@@ -12,15 +12,13 @@
 
 int main(int argc, char** argv)
 {
-    if (argc != 2)
-  {
+  if (argc != 2) {
     std::cerr << "Incorrect parameters\n";
     return 1;
   }
   std::vector< petrov::Polygon > polygons;
   std::ifstream file(argv[1]);
-  if (!file)
-  {
+  if (!file) {
     std::cerr << "Error: cannot open file\n";
     return 1;
   }
@@ -28,11 +26,9 @@ int main(int argc, char** argv)
   constexpr std::streamsize streamMax = std::numeric_limits< std::streamsize >::max();
   using iit_t = std::istream_iterator< petrov::Polygon >;
 
-  while (!file.eof())
-  {
+  while (!file.eof()) {
     std::copy(iit_t(file), iit_t(), std::back_inserter(polygons));
-    if (!file)
-    {
+    if (!file) {
       file.clear();
       file.ignore(streamMax, '\n');
     }
@@ -46,4 +42,18 @@ int main(int argc, char** argv)
   commands["PERMS"] = std::bind(petrov::perms, std::ref(std::cin), std::ref(std::cout), std::cref(polygons));
   commands["INFRAME"] = std::bind(petrov::inframe, std::ref(std::cin), std::ref(std::cout), std::cref(polygons));
 
+  std::string command;
+  while (std::cin >> command) {
+    try {
+      commands.at(command)();
+    } catch (const std::exception&) {
+      if (std::cin.fail()) {
+        std::cin.clear();
+      }
+      std::cin.ignore(streamMax, '\n');
+      std::cout << "<INVALID COMMAND>\n";
+    }
+  }
+
+  return 0;
 }
