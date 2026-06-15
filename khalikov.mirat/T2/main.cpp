bccfb34d8a2d5b8397290292ac6da4761596e163
khalii21 <miratkhalikov007@gmail.com>
2026-06-15 15:55:05 +0300

    impl of istreams

--- khalikov.mirat/T2/main.cpp
+++ khalikov.mirat/T2/main.cpp
@@ -5,9 +5,8 @@
 #include <iterator>
 #include <vector>
 #include <iomanip>
+#include <limits>
 
-//DBL LIT
-//ULL OCT
 namespace khalikov {
 
 	struct DataStruct {
@@ -48,9 +47,10 @@ namespace khalikov {
       ~IoGuard();
     private:
       std::basic_ios< char >& s_;
-      char fill_;
+      std::streamsize width_;
       std::streamsize precision_;
       std::basic_ios< char >::fmtflags fmt_;
+      char fill_;
    };
 
   bool operator<(const DataStruct& lhs, const DataStruct& rhs);
@@ -68,7 +68,7 @@ int main() {
   {
     using iit_t = std::istream_iterator< khalikov::DataStruct >;
     while (!std::cin.eof()) {
-      std::copy(iit_t(std::cin), iit_t(), std::back_inserter(data))
+      std::copy(iit_t(std::cin), iit_t(), std::back_inserter(data));
       if (std::cin.fail() && !std::cin.eof()) {
         std::cin.clear();
         auto toIgnore = std::numeric_limits< std::streamsize >::max();
@@ -79,11 +79,66 @@ int main() {
   std::sort(data.begin(), data.end());
   {
     using oit_t = std::ostream_iterator<khalikov::DataStruct>;
-    std::copy(data.begin(), data.end(), oit_t(std::cout, '\n'));
+    std::copy(data.begin(), data.end(), oit_t(std::cout, "\n"));
   }
 }
 
-std::istream& operator>>(std::istream& in, DelimiterIO&& dest) {
+std::istream& khalikov::operator>>(std::istream& in, KeyIO&& dest) {
+  std::istream::sentry s(in);
+  if (!s) {
+    return in;
+  }
+  std::string curr = "";
+  in >> curr;
+  if (curr == "key1") {
+    dest.ref = Key::KEY1;
+  }
+  else if (curr == "key2") {
+    dest.ref = Key::KEY2;
+  }
+  else if (curr == "key3") {
+    dest.ref = Key::KEY3;
+  }
+  else {
+    in.setstate(std::ios::failbit);
+  }
+  return in;
+}
+
+std::istream& khalikov::operator>>(std::istream& in, StringIO&& dest) {
+  std::istream::sentry s(in);
+  if (!s) {
+    return in;
+  }
+  return std::getline(in >> DelimiterIO{ '"' }, dest.ref, '"');
+}
+
+std::istream& khalikov::operator>>(std::istream& in, UllIO&& dest) {
+  std::istream::sentry s(in);
+  if (!s) {
+    return in;
+  }
+  IoGuard fmtguard(in);
+  in >> std::oct >> dest.ref >> std::dec;
+  return in;
+}
+
+std::istream& khalikov::operator>>(std::istream& in, DoubleIO&& dest) {
+  std::istream::sentry s(in);
+  if (!s) {
+    return in;
+  }
+  in >> dest.ref;
+  char suf = '0';
+  in >> suf;
+  if (in && (suf != 'd' && suf != 'D')) {
+    in.setstate(std::ios::failbit);
+  }
+  return in;
+}
+
+
+std::istream& khalikov::operator>>(std::istream& in, DelimiterIO&& dest) {
   std::istream::sentry s(in);
   if (!s) {
     return in;
@@ -113,7 +168,7 @@ std::ostream& khalikov::operator<<(std::ostream& out, const DataStruct& src) {
 
 khalikov::IoGuard::IoGuard(std::basic_ios< char >& s):
   s_(s),
-  width(s.width()),
+  width_(s.width()),
   fill_(s.fill()),
   precision_(s.precision()),
   fmt_(s.flags())
