a2e7e8ad8696cb42ae05f16a146ca3683edef2af
khalii21 <miratkhalikov007@gmail.com>
2026-06-15 13:50:40 +0300

    impl of ioguard

--- khalikov.mirat/T2/main.cpp
+++ khalikov.mirat/T2/main.cpp
@@ -82,6 +82,19 @@ int main() {
   }
 }
 
+khalikov::IoGuard::IoGuard(std::basic_ios< char >& s):
+  s_(s),
+  width(s.width()),
+  fill_(s.fill()),
+  precision_(s.precision()),
+  fmt_(s.flags())
+{}
 
+khalikov::IoGuard::~IoGuard() {
+  s_.width(width_);
+  s_.fill(fill_);
+  s_.precision(precision_);
+  s_.flags(fmt_);
+}
 
 
