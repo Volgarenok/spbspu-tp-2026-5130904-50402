aee0f1b76e1b740ffa9a190ab8ea95a90271d00b
s1nhronn <silekatik@yandex.ru>
2026-06-18 15:55:11 +0300

    Fix MAXSEQ №2 (last)

--- shirokov.arsenii/T3/maxseq.cpp
+++ shirokov.arsenii/T3/maxseq.cpp
@@ -1,7 +1,9 @@
 #include <algorithm>
+#include <cctype>
 #include <iomanip>
 #include <iostream>
 #include <stdexcept>
+#include <string>
 #include <vector>
 #include "commands.hpp"
 #include "parser.hpp"
@@ -10,6 +12,7 @@ namespace
 {
   bool isPointEqual(const shirokov::Point&, const shirokov::Point&);
   bool isPolygonEqual(const shirokov::Polygon&, const shirokov::Polygon&);
+  bool isNotSpace(char);
 
   struct SequenceTracker
   {
@@ -56,6 +59,11 @@ namespace
     }
     return std::equal(a.points.begin(), a.points.end(), b.points.begin(), isPointEqual);
   }
+
+  bool isNotSpace(char c)
+  {
+    return std::isspace(static_cast< unsigned char >(c)) == 0;
+  }
 }
 
 void shirokov::maxseq(std::istream& in, std::ostream& out, shirokov::plg_t& polygons)
@@ -65,6 +73,17 @@ void shirokov::maxseq(std::istream& in, std::ostream& out, shirokov::plg_t& poly
   {
     in.clear();
     throw std::logic_error("Invalid polygon format");
+  }
+
+  std::string restOfLine;
+  if (std::getline(in, restOfLine))
+  {
+    if (std::any_of(restOfLine.begin(), restOfLine.end(), isNotSpace))
+    {
+      in.clear();
+      in.putback('\n');
+      throw std::logic_error("Trailing garbage after polygon");
+    }
   }
 
   shirokov::IOguard g(out);
