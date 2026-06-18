61f3585792bcb0c847084ce0c6d349ba8984fbd1
InsafM1 <muhamadiarovinsaf0@gmail.com>
2026-06-18 23:19:51 +0300

    Create interface polygon

---
+++ muhamadiarov.insaf/T3/polygon.hpp
@@ -0,0 +1,23 @@
+#ifndef POLYGON_HPP
+#define POLYGON_HPP
+
+#include <vector>
+#include <iostream>
+
+namespace muhamadiarov
+{
+  struct Point
+  {
+    int x, y;
+  };
+
+  struct Polygon
+  {
+    std::vector< Point > points;
+  };
+
+  std::istream& operator>>(std::istream& in, Point& point);
+  std::istream& operator>>(std::istream& in, Polygon& polygon);
+}
+
+#endif
