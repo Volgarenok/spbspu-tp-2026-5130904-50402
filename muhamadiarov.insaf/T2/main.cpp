61dc9dc278231b213d2e6c4463ea0e4a694afb90
InsafM1 <muhamadiarovinsaf0@gmail.com>
2026-06-18 15:41:29 +0300

    implemented a task for the laboratory

--- muhamadiarov.insaf/T2/main.cpp
+++ muhamadiarov.insaf/T2/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+#include <limits>
+
+namespace muhamadiarov
+{
+  struct DataStruct;
+}
 
 int main()
 {
+  using T = muhamadiarov::DataStruct;
+  std::vector< T > data;
+
+  using iit_t = std::istream_iterator< T >;
+  while (!std::cin.eof())
+  {
+    std::copy(iit_t{std::cin}, iit_t{}, std::back_inserter(data));
+    if (std::cin.fail())
+    {
+      std::cin.clear(std::cin.rdstate() & ~std::ios::failbit);
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+  }
+
+  std::sort(data.begin(), data.end());
+
+  using oit_t = std::ostream_iterator< T >;
+
+  std::copy(data.begin(), data.end(), oit_t{std::cout, '\n'});
   return 0;
 }
