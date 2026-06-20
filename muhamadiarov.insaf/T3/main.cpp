4da3801b9c3b942de686ea7687609b32039ce1dd
InsafM1 <muhamadiarovinsaf0@gmail.com>
2026-06-20 17:11:41 +0300

    Done main

--- muhamadiarov.insaf/T3/main.cpp
+++ muhamadiarov.insaf/T3/main.cpp
@@ -1,6 +1,66 @@
+#include <algorithm>
+#include <fstream>
+#include <functional>
 #include <iostream>
+#include <iterator>
+#include <limits>
+#include <map>
+#include <string>
+#include <vector>
+#include "commands.hpp"
 
-int main()
+int main(int argc, char **argv)
 {
+  namespace muh = muhamadiarov;
+  using iit_t = std::istream_iterator< muh::Polygon >;
+  if (argc != 2)
+  {
+    std::cerr << "Incorrect parameters\n";
+    return 1;
+  }
+
+  std::ifstream file(argv[1]);
+  if (!file)
+  {
+    std::cerr << "Error: cannot open file\n";
+    return 1;
+  }
+
+  std::vector< muh::Polygon > polygons;
+  while (!file.eof())
+  {
+    std::copy(iit_t(file), iit_t(), std::back_inserter(polygons));
+    if (!file) {
+      file.clear();
+      file.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+  }
+
+  std::map< std::string, std::function< void() > > commands;
+  commands["RIGHTSHAPES"] = std::bind(muh::rightshapes, std::ref(std::cin), std::ref(std::cout), std::cref(polygons));
+  commands["INTERSECTIONS"] = std::bind(muh::intersections, std::ref(std::cin), std::ref(std::cout), std::cref(polygons));
+  commands["AREA"] = std::bind(muh::area, std::ref(std::cin), std::ref(std::cout), std::cref(polygons));
+  commands["MAX"] = std::bind(muh::max, std::ref(std::cin), std::ref(std::cout), std::cref(polygons));
+  commands["MIN"] = std::bind(muh::min, std::ref(std::cin), std::ref(std::cout), std::cref(polygons));
+  commands["COUNT"] = std::bind(muh::count, std::ref(std::cin), std::ref(std::cout), std::cref(polygons));
+  
+  std::string command;
+  while (std::cin >> command)
+  {
+    try
+    {
+      commands.at(command)();
+      std::cout << '\n';
+    } 
+    catch (const std::exception &)
+    {
+      if (std::cin.fail())
+      {
+        std::cin.clear();
+      }
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+      std::cout << "<INVALID COMMAND>\n";
+    }
+  }
   return 0;
 }
