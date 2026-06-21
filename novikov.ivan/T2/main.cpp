1c59ae2941ca7779132446771016951057149256
Kayv1e <128278935+Kayv1e@users.noreply.github.com>
2026-06-21 21:15:27 +0300

    Implemented operator >> for double, complex and string

--- novikov.ivan/T2/main.cpp
+++ novikov.ivan/T2/main.cpp
@@ -287,3 +287,54 @@ std::istream& novikov::operator>>(std::istream& in, label&& dest)
   dest.used.push_back(inputType);
   return in;
 }
+
+std::istream& novikov::operator>>(std::istream& in, dbl_lit&& dest)
+{
+  std::istream::sentry s(in);
+  if (!s)
+  {
+    return in;
+  }
+
+  double val = 0.0;
+  in >> val >> uncase_sep{'d'};
+
+  if (in)
+  {
+    dest.ref = val;
+  }
+  return in;
+}
+
+std::istream& novikov::operator>>(std::istream& in, cmp_lsp&& dest)
+{
+  std::istream::sentry s(in);
+  if (!s)
+  {
+    return in;
+  }
+
+  double real, imag;
+
+  in >> sep{'#'} >> sep{'c'} >> sep{'('};
+  in >> real;
+  in >> sep{' '};
+  in >> imag;
+  in >> sep{')'};
+
+  if (in)
+  {
+    dest.ref = std::complex<double>(real, imag);
+  }
+  return in;
+}
+
+std::istream& novikov::operator>>(std::istream& in, str&& dest)
+{
+  std::istream::sentry s(in);
+  if (!s)
+  {
+    return in;
+  }
+  return std::getline(in >> sep{'"'}, dest.ref, '"');
+}
