6a50fed6ec1515eac36dca231a703c40eec29680
Kayv1e <128278935+Kayv1e@users.noreply.github.com>
2026-06-21 20:30:25 +0300

    Implemented IOGuard

--- novikov.ivan/T2/main.cpp
+++ novikov.ivan/T2/main.cpp
@@ -182,3 +182,19 @@ std::istream& novikov::operator>>(std::istream& in, key&& dest)
   }
   return in;
 }
+
+novikov::IOguard::IOguard(std::basic_ios< char >& s):
+    s_(s),
+    width_(s.width()),
+    precision_(s.precision()),
+    fmt_(s.flags()),
+    fill_(s.fill())
+{}
+
+novikov::IOguard::~IOguard()
+{
+  s_.width(width_);
+  s_.fill(fill_);
+  s_.precision(precision_);
+  s_.flags(fmt_);
+}
