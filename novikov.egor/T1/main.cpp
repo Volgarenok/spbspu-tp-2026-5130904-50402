d7a7fc37eea69b09543fadd2c247a4492c106f7a
EGGorkaa <egor_novikov20004@mail.ru>
2026-06-24 16:19:10 +0300

    fix main

--- novikov.egor/T1/main.cpp
+++ novikov.egor/T1/main.cpp
@@ -21,7 +21,6 @@ int main()
   while (std::cin >> cmd) {
     try {
       cmds.at(cmd)(std::cin, std::cout, db);
-      std::cout << "\n";
     } catch (const std::exception &) {
       std::cout << "<INVALID COMMAND>\n";
       auto toignore = std::numeric_limits< std::streamsize >::max();
