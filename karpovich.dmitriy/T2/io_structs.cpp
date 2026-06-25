98ecd2d73a7e0b7195be6cbb5a3c88eb2944cd96
voyadger01 <voyadgerkx@gmail.com>
2026-06-25 17:55:38 +0300

    fix

--- karpovich.dmitriy/T2/io_structs.cpp
+++ karpovich.dmitriy/T2/io_structs.cpp
@@ -35,10 +35,9 @@ std::istream &karpovich::operator>>(std::istream &in, BinIO &&dest)
     return in;
   }
   std::string numBin;
-  char c = '\0';
-  while (in.peek() == '0' || in.peek() == '1') {
-    in >> c;
-    numBin.push_back(c);
+  bool isOne = false;
+  while (in >> DelimIO{'0', '1', &isOne}) {
+    numBin.push_back(isOne ? '1' : '0');
   }
   if (numBin.empty()) {
     in.setstate(std::ios::failbit);
@@ -82,24 +81,33 @@ std::istream &karpovich::operator>>(std::istream &in, DataStruct &dest)
   bool got1 = false;
   bool got2 = false;
   bool got3 = false;
+  bool consumed = false;
   in >> DelimIO{'('};
   if (!in) {
     return in;
   }
   while ((!got1 || !got2 || !got3) && in) {
+    if (!consumed) {
+      in >> DelimIO{':'};
+    }
+    consumed = false;
     std::string label;
     in >> label;
-    if (label == ":key1" && !got1) {
+    if (label == "key1" && !got1) {
       in >> BinIO{temp.key1};
+      if (!in) {
+        consumed = true;
+        in.clear(in.rdstate() & ~std::ios::failbit);
+      }
       if (in) {
         got1 = true;
       }
-    } else if (label == ":key2" && !got2) {
+    } else if (label == "key2" && !got2) {
       in >> OctIO{temp.key2};
       if (in) {
         got2 = true;
       }
-    } else if (label == ":key3" && !got3) {
+    } else if (label == "key3" && !got3) {
       in >> StringIO{temp.key3};
       if (in) {
         got3 = true;
@@ -108,7 +116,10 @@ std::istream &karpovich::operator>>(std::istream &in, DataStruct &dest)
       in.setstate(std::ios::failbit);
     }
   }
-  in >> DelimIO{':'} >> DelimIO{')'};
+  if (!consumed) {
+    in >> DelimIO{':'};
+  }
+  in >> DelimIO{')'};
   if (in && got1 && got2 && got3) {
     dest = temp;
   } else {
