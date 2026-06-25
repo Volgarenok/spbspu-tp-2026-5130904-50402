35a5c8c08cd83f5b9b6dc42e73919b1a77f51c9a
sp0146001 <sp0146001@gmail.com>
2026-06-25 21:23:39 +0300

    6 spaces

--- petrov.sasha/T1/note.hpp
+++ petrov.sasha/T1/note.hpp
@@ -33,7 +33,7 @@ namespace petrov
   void countExpired(std::istream &is, std::ostream &os, notes_t &db);
   void cmdLoop(std::istream &is, std::ostream &os, notes_t &db);
   bool findLoop(const std::string &startName, const std::string &currentName,
-                 size_t depthLeft, notes_t &db, std::vector< std::string > &path);
+      size_t depthLeft, notes_t &db, std::vector< std::string > &path);
   using linkIt_t = std::vector< std::weak_ptr< Note > >::iterator;
   linkIt_t find(linkIt_t start, linkIt_t end, const std::string& name);
 }
