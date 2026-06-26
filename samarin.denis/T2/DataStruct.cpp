b37836a62657988bac79693f4dd52424ef439455
DalveShutnik <119686844+DalveShutnik@users.noreply.github.com>
2026-06-26 18:08:40 +0300

    read record labels and braces

--- samarin.denis/T2/DataStruct.cpp
+++ samarin.denis/T2/DataStruct.cpp
@@ -1,9 +1,45 @@
 #include "DataStruct.hpp"
 
+#include <cstddef>
+#include <istream>
 #include <string>
+#include "DelimiterIO.hpp"
 
 namespace {
+  using samarin::DelimiterIO;
+
   struct LabelIO {
     std::string &ref;
   };
+
+  std::istream &operator>>(std::istream &in, LabelIO &&dest)
+  {
+    std::istream::sentry sentry(in);
+    if (!sentry) {
+      return in;
+    }
+    in >> dest.ref;
+    return in;
+  }
+}
+
+std::istream &samarin::operator>>(std::istream &in, DataStruct &dest)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry) {
+    return in;
+  }
+  const std::size_t fieldCount = 3;
+  DataStruct input{};
+  in >> DelimiterIO{'('};
+  for (std::size_t i = 0; i < fieldCount; ++i) {
+    in >> DelimiterIO{':'};
+    std::string label;
+    in >> LabelIO{label};
+    in.setstate(std::ios::failbit);
+  }
+  if (in) {
+    dest = input;
+  }
+  return in;
 }
