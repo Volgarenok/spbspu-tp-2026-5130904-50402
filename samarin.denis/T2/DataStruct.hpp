9245aefe2c716c9ac9e2c8b385b275f3a6b4cff4
DalveShutnik <119686844+DalveShutnik@users.noreply.github.com>
2026-06-26 16:54:22 +0300

    define DataStruct type

---
+++ samarin.denis/T2/DataStruct.hpp
@@ -0,0 +1,15 @@
+#ifndef DATA_STRUCT_HPP
+#define DATA_STRUCT_HPP
+
+#include <iosfwd>
+#include <string>
+
+namespace samarin {
+  struct DataStruct {
+    unsigned long long key1;
+    unsigned long long key2;
+    std::string key3;
+  };
+}
+
+#endif
