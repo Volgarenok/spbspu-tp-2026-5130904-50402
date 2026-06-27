75aba008a8473df063a5dd633b89df7460dc390a
DalveShutnik <119686844+DalveShutnik@users.noreply.github.com>
2026-06-27 22:09:46 +0300

    read polygons from the file

--- samarin.denis/T3/main.cpp
+++ samarin.denis/T3/main.cpp
@@ -1,5 +1,10 @@
+#include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <iterator>
+#include <limits>
+#include <vector>
+#include "Polygon.hpp"
 
 int main(int argc, char **argv)
 {
@@ -11,5 +16,16 @@ int main(int argc, char **argv)
   if (!file) {
     std::cerr << "ERROR: cannot open the input file\n";
     return 2;
+  }
+
+  using samarin::Polygon;
+  using input_iterator = std::istream_iterator< Polygon >;
+  std::vector< Polygon > polygons;
+  while (!file.eof()) {
+    if (file.fail()) {
+      file.clear();
+      file.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+    std::copy(input_iterator(file), input_iterator(), std::back_inserter(polygons));
   }
 }
