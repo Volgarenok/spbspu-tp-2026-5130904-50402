e7fc168bcbe00385f04c1611f51aeee5235d4182
DalveShutnik <119686844+DalveShutnik@users.noreply.github.com>
2026-06-27 11:37:26 +0300

    read records via STL iterators

---
+++ samarin.denis/T2/main.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <vector>
+#include "DataStruct.hpp"
+
+int main()
+{
+  using samarin::DataStruct;
+  using input_iterator = std::istream_iterator< DataStruct >;
+
+  std::vector< DataStruct > data;
+  while (!std::cin.eof()) {
+    if (std::cin.fail()) {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+    std::copy(input_iterator(std::cin), input_iterator(), std::back_inserter(data));
+  }
+}
