fd9c229614e7732adba0c03c1aca91a64f9d23c9
kuzRK <sanalsogdanov11188@gmail.com>
2026-06-27 16:16:38 +0300

    fix

--- sogdanov.sanal/T2/DataStruct.cpp
+++ sogdanov.sanal/T2/DataStruct.cpp
@@ -1,53 +1,10 @@
 #include "DataStruct.hpp"
+#include "IoGuard.hpp"
 #include <iomanip>
 #include <string>
 
 namespace sogdanov
 {
-  struct DelimiterIO
-  {
-    char exp;
-  };
-
-  struct DblLitIO
-  {
-    double &ref;
-  };
-
-  struct SllLitIO
-  {
-    long long &ref;
-  };
-
-  struct StringIO
-  {
-    std::string &ref;
-  };
-
-  enum class Key
-  {
-    KEY1,
-    KEY2,
-    KEY3
-  };
-
-  struct KeyIO
-  {
-    Key &ref;
-  };
-
-  class IoGuard
-  {
-  public:
-    explicit IoGuard(std::basic_ios<char> &s);
-    ~IoGuard();
-
-  private:
-    std::basic_ios<char> &s_;
-    char fill_;
-    std::streamsize precision_;
-    std::basic_ios<char>::fmtflags fmt_;
-  };
   std::istream& operator>>(std::istream& in, DelimiterIO&& dest)
   {
     std::istream::sentry sentry(in);
@@ -127,6 +84,7 @@ namespace sogdanov
     }
     return in;
   }
+
   std::istream& operator>>(std::istream& in, DataStruct& dest)
   {
     std::istream::sentry sentry(in);
@@ -204,19 +162,5 @@ namespace sogdanov
       return lhs.key2 < rhs.key2;
     }
     return lhs.key3.length() < rhs.key3.length();
-  }
-
-  IoGuard::IoGuard(std::basic_ios<char> &s):
-   s_(s),
-   fill_(s.fill()),
-   precision_(s.precision()),
-   fmt_(s.flags())
-  {}
-
-  IoGuard::~IoGuard()
-  {
-    s_.fill(fill_);
-    s_.precision(precision_);
-    s_.flags(fmt_);
   }
 }
