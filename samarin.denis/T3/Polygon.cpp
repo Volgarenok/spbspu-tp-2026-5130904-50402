f9ce6f2e4ec298909f03c100dba1e4c2e2163208
DalveShutnik <119686844+DalveShutnik@users.noreply.github.com>
2026-06-27 19:26:44 +0300

    parse a polygon line strictly

--- samarin.denis/T3/Polygon.cpp
+++ samarin.denis/T3/Polygon.cpp
@@ -1,7 +1,23 @@
 #include "Polygon.hpp"
 
+#include <algorithm>
+#include <cstddef>
 #include <istream>
+#include <iterator>
+#include <vector>
 #include "DelimiterIO.hpp"
+
+namespace {
+  bool restOfLineIsBlank(std::istream &in)
+  {
+    int next = in.peek();
+    while (next == ' ' || next == '\t') {
+      in.get();
+      next = in.peek();
+    }
+    return (next == '\n') || (next == std::char_traits< char >::eof());
+  }
+}
 
 std::istream &samarin::operator>>(std::istream &in, Point &dest)
 {
@@ -13,6 +29,29 @@ std::istream &samarin::operator>>(std::istream &in, Point &dest)
   in >> DelimiterIO{'('} >> input.x >> DelimiterIO{';'} >> input.y >> DelimiterIO{')'};
   if (in) {
     dest = input;
+  }
+  return in;
+}
+
+std::istream &samarin::operator>>(std::istream &in, Polygon &dest)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry) {
+    return in;
+  }
+  std::size_t count = 0;
+  in >> count;
+  if (!in || (count < 3)) {
+    in.setstate(std::ios::failbit);
+    return in;
+  }
+  std::vector< Point > input;
+  input.reserve(count);
+  std::copy_n(std::istream_iterator< Point >(in), count, std::back_inserter(input));
+  if (in && (input.size() == count) && restOfLineIsBlank(in)) {
+    dest.points = input;
+  } else {
+    in.setstate(std::ios::failbit);
   }
   return in;
 }
