e699c0564767f78a6192ade37c42d607a62398f0
sp0146001 <sp0146001@gmail.com>
2026-06-29 01:04:55 +0300

    remove helpers to the common directory + writing functions, using full name

--- petrov.sasha/T3/shapes.hpp
+++ petrov.sasha/T3/shapes.hpp
@@ -1,6 +1,7 @@
 #ifndef SHAPES_HPP
 #define SHAPES_HPP
 
+#include <cstddef>
 #include <istream>
 #include <vector>
 
@@ -8,49 +9,31 @@ namespace petrov
 {
   struct Point
   {
-    int x, y;
+    int x;
+    int y;
   };
 
-  bool operator==(const Point& lhs, const Point& rhs);
+  bool operator==(const Point &lhs, const Point &rhs);
 
   struct Polygon
   {
     std::vector< Point > points;
   };
 
-  struct DelimIO
-  {
-    char expected;
-  };
+  std::istream &operator>>(std::istream &in, Point &point);
+  std::istream &operator>>(std::istream &in, Polygon &polygon);
 
-  std::istream& operator>>(std::istream& in, DelimIO&& delim);
+  bool compareByX(const Point &a, const Point &b);
+  bool compareByY(const Point &a, const Point &b);
+  bool comparePoints(const Point &a, const Point &b);
+  Point translatePoint(const Point &p, int dx, int dy);
+  Point swapPointXY(const Point &p);
+  double crossTerm(const std::vector< Point > &pts, std::size_t i, std::size_t n);
 
-  class IOguard
-  {
-  public:
-    explicit IOguard(std::istream& stream);
-    ~IOguard();
-
-  private:
-    std::istream& stream_;
-    std::ios::fmtflags flags_;
-  };
-
-  std::istream& operator>>(std::istream& in, Point& point);
-  std::istream& operator>>(std::istream& in, Polygon& polygon);
-
-  bool compareByX(const Point& a, const Point& b);
-  bool compareByY(const Point& a, const Point& b);
-  bool comparePoints(const Point& a, const Point& b);
-  Point translatePoint(const Point& p, int dx, int dy);
-  Point swapPointXY(const Point& p);
-  double crossTerm(const std::vector< Point >& pts, size_t i, size_t n);
-
-  double calculateArea(const Polygon& poly);
-  Polygon swapCoordinates(const Polygon& poly);
-  std::vector< Point > normalize(const Polygon& poly);
-  bool isPermutationOf(const Polygon& a, const Polygon& b);
+  double calculateArea(const Polygon &poly);
+  Polygon swapCoordinates(const Polygon &poly);
+  std::vector< Point > normalize(const Polygon &poly);
+  bool isPermutationOf(const Polygon &a, const Polygon &b);
 }
 
 #endif
-
