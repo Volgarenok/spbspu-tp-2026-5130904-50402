14691c6157d8488a909c6809dd78a0b3cbea4425
Kayv1e <128278935+Kayv1e@users.noreply.github.com>
2026-06-30 02:07:58 +0300

    Removed second IOGuard definition in DataStruct

--- novikov.ivan/T2/DataStruct.cpp
+++ novikov.ivan/T2/DataStruct.cpp
@@ -79,22 +79,6 @@ std::istream& novikov::operator>>(std::istream& in, key&& dest)
   return in;
 }
 
-novikov::IOGuard::IOGuard(std::basic_ios< char >& s):
-    s_(s),
-    width_(s.width()),
-    precision_(s.precision()),
-    fmt_(s.flags()),
-    fill_(s.fill())
-{}
-
-novikov::IOGuard::~IOGuard()
-{
-  s_.width(width_);
-  s_.fill(fill_);
-  s_.precision(precision_);
-  s_.flags(fmt_);
-}
-
 bool novikov::compare(const DataStruct& lhs, const DataStruct& rhs)
 {
   if (lhs.key1 != rhs.key1)
