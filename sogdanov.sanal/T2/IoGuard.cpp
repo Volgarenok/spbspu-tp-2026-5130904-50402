698868331bc64eee9cdb0a771b75a6557924ccea
kuzRK <sanalsogdanov11188@gmail.com>
2026-06-29 16:05:40 +0300

    fix CG

--- sogdanov.sanal/T2/IoGuard.cpp
+++ sogdanov.sanal/T2/IoGuard.cpp
@@ -1,18 +1,15 @@
 #include "IoGuard.hpp"
 
-namespace sogdanov
-{
-  IoGuard::IoGuard(std::basic_ios< char >& s):
-    s_(s),
-    fill_(s.fill()),
-    precision_(s.precision()),
-    fmt_(s.flags())
-  {}
+sogdanov::IoGuard::IoGuard(std::basic_ios< char >& s):
+  s_(s),
+  fill_(s.fill()),
+  precision_(s.precision()),
+  fmt_(s.flags())
+{}
 
-  IoGuard::~IoGuard()
-  {
-    s_.fill(fill_);
-    s_.precision(precision_);
-    s_.flags(fmt_);
-  }
+sogdanov::IoGuard::~IoGuard()
+{
+  s_.fill(fill_);
+  s_.precision(precision_);
+  s_.flags(fmt_);
 }
