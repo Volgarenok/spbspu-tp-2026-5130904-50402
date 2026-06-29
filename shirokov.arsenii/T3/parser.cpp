aed894652941c963b22f29fb26d4faa6edcbdc9d
s1nhronn <silekatik@yandex.ru>
2026-06-29 23:08:59 +0300

    Remove sstream

--- shirokov.arsenii/T3/parser.cpp
+++ shirokov.arsenii/T3/parser.cpp
@@ -1,14 +1,13 @@
 #include "parser.hpp"
 #include <algorithm>
 #include <iterator>
-#include <sstream>
 #include <vector>
 #include "DelimiterIO.hpp"
 #include "IOguard.hpp"
 
 std::istream& shirokov::operator>>(std::istream& in, Point& pt)
 {
-  in >> DelimiterIO{' '} >> DelimiterIO{'('} >> pt.x >> DelimiterIO{';'} >> pt.y >> DelimiterIO{')'};
+  in >> DelimiterIO{'('} >> pt.x >> DelimiterIO{';'} >> pt.y >> DelimiterIO{')'};
   return in;
 }
 
@@ -33,8 +32,6 @@ std::istream& shirokov::operator>>(std::istream& in, PolygonIO&& dest)
     return in;
   }
 
-  in >> std::noskipws;
-
   std::vector< Point > temp_points;
   temp_points.reserve(count);
 
@@ -52,49 +49,9 @@ std::istream& shirokov::operator>>(std::istream& in, Polygon& p)
   return in >> PolygonIO{p};
 }
 
-std::istream& shirokov::operator>>(std::istream& in, Line& dest)
-{
-  std::getline(in, dest.lineText);
-  return in;
-}
-
-shirokov::PolygonLineInserter::PolygonLineInserter(plg_t& containerVec):
-  container(containerVec)
-{}
-
-shirokov::PolygonLineInserter& shirokov::PolygonLineInserter::operator=(const Line& line)
-{
-  std::istringstream iss(line.lineText);
-  Polygon poly;
-  if (iss >> poly)
-  {
-    char ch = 0;
-    if (!(iss >> ch))
-    {
-      container.push_back(poly);
-    }
-  }
-  return *this;
-}
-
-shirokov::PolygonLineInserter& shirokov::PolygonLineInserter::operator*()
-{
-  return *this;
-}
-
-shirokov::PolygonLineInserter& shirokov::PolygonLineInserter::operator++()
-{
-  return *this;
-}
-
-shirokov::PolygonLineInserter& shirokov::PolygonLineInserter::operator++(int)
-{
-  return *this;
-}
-
 shirokov::plg_t shirokov::parse(std::istream& in)
 {
   plg_t data;
-  std::copy(std::istream_iterator< Line >(in), std::istream_iterator< Line >(), PolygonLineInserter{data});
+  std::copy(std::istream_iterator< Polygon >(in), std::istream_iterator< Polygon >(), std::back_inserter(data));
   return data;
 }
