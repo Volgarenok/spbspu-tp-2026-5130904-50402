06a5e0c2a1ad103c69a409b4cbd068fca3edf03c
m0trex <timofeyalisov@gmail.com>
2026-06-30 04:52:01 +0300

    work on main

--- alisov.timofey/T2/main.cpp
+++ alisov.timofey/T2/main.cpp
@@ -1,4 +1,20 @@
-#include <cstddef>
+#include <algorithm>
+#include <iterator>
+#include <limits>
+#include <vector>
+#include "DataStruct.hpp"
 
 int main()
-{}
+{
+  std::vector< alisov::DataStruct > data;
+
+  while (!std::cin.eof()) {
+    std::copy(std::istream_iterator< alisov::DataStruct >(std::cin), std::istream_iterator< alisov::DataStruct >(),
+              std::back_inserter(data));
+
+    if (std::cin.fail() && !std::cin.eof()) {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+  }
+}
