a5b59dccc16bbb39f80bf344473207deca4b7706
m0trex <timofeyalisov@gmail.com>
2026-06-30 04:12:48 +0300

    working on operator<<

--- alisov.timofey/T2/DataStruct.cpp
+++ alisov.timofey/T2/DataStruct.cpp
@@ -1,5 +1,6 @@
 #include "DataStruct.hpp"
 #include <iomanip>
+#include <sstream>
 
 bool alisov::operator<(const DataStruct &lhs, const DataStruct &rhs)
 {
@@ -20,5 +21,31 @@ std::ostream &alisov::operator<<(std::ostream &out, const DataStruct &src)
   }
 
   out << "(:key1 " << std::fixed << std::setprecision(1) << src.key1 << "d";
+  out << ":key2 ";
+
+  auto flags = out.flags();
+  std::stringstream ss;
+  ss << std::scientific << std::setprecision(2) << src.key2;
+  std::string sci_str = ss.str();
+
+  size_t e_pos = sci_str.find('e');
+  if (e_pos != std::string::npos) {
+    out << sci_str.substr(0, e_pos + 1);
+    if (sci_str[e_pos + 1] != '+' && sci_str[e_pos + 1] != '-') {
+      out << "+";
+    }
+
+    std::string exp_val = sci_str.substr(e_pos + 1);
+    if (exp_val[0] == '+' || exp_val[0] == '-') {
+      out << exp_val[0];
+      exp_val = exp_val.substr(1);
+    }
+    long long exp_num = std::stoll(exp_val);
+    out << exp_num;
+  } else {
+    out << sci_str;
+  }
+
+  out.flags(flags);
   return out;
 }
